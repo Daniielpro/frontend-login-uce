8f7f92384e8dad988acb3301a2ab0f9a9a5802bf
Daniielpro <proanodaniel20@gmail.com>
2024-05-07 01:27:17 -0500

    Update authApi.js

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://backend-login-uce-d6aab1c9d120.herokuapp.com/api';
+const BASE_URL = 'https://backend-login-uce-283a6699bf54.herokuapp.com/api';
 
 
 export const registerUser = async (userData) => {
